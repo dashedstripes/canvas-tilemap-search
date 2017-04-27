a11b114eb297bef48ab9990186076f06d372e416
Adam Gray <adam@pateo.co>
2017-04-27 16:46:26 +0100

    Add black flag behind tile name

--- dist/bundle.js
+++ dist/bundle.js
@@ -330,9 +330,14 @@ class Info {
   }
 
   draw() {
-    this.context.fillStyle = '#ffffff'
+    let textWidth = this.context.measureText(this.text).width
+    let textHeight = 18
+    
     this.context.font = "18px Open Sans";
-    this.context.fillText(this.text, this.context.canvas.width - (this.context.measureText(this.text).width + 20), 30)
+    this.context.fillStyle = '#000000'
+    this.context.fillRect(this.context.canvas.width - (textWidth + 20) - 20, 20, textWidth * 2, textHeight * 2)
+    this.context.fillStyle = '#ffffff'
+    this.context.fillText(this.text, this.context.canvas.width - (textWidth + 20), 45)
   }
 }
 
