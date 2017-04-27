4badeef5ff40fbcf3b5c3df914b491f191062abc
Adam Gray <adam@pateo.co>
2017-04-27 11:27:44 +0100

    Add more grass

--- dist/bundle.js
+++ dist/bundle.js
@@ -186,19 +186,33 @@ class Map {
     this.x = this.y = 1
     this.vx = this.vy = 0
     this.width = this.height = 0
-    this.rows = 40
-    this.cols = 60
+    this.rows = this.cols = 100
 
-    this.tiles = {
-      grass: new Tile(this.context, '#2ecc71')
-    }
+    this.tiles = [
+      {
+        name: 'grass',
+        tile: new Tile(this.context, '#27ae60')
+      },
+      {
+        name: 'grass2',
+        tile: new Tile(this.context, '#2ecc71')
+      },
+      {
+        name: 'grass3',
+        tile: new Tile(this.context, '#00B16A')
+      },
+      {
+        name: 'grass4',
+        tile: new Tile(this.context, '#26C281')
+      }
+    ]
 
     this.map = this.createMap()
 
     if(this.map != null && this.map.length > 0) {
-      this.height += this.map.length * (this.tiles.grass.height + 1)
+      this.height += this.map.length * (this.tiles[0].tile.height + 1)
       if(this.map[0] != null && this.map[0].length > 0) {
-        this.width += this.map[0].length * (this.tiles.grass.width + 1)
+        this.width += this.map[0].length * (this.tiles[0].tile.width + 1)
       }
     }
   }
@@ -208,7 +222,7 @@ class Map {
     for(let i = 0; i < this.rows; i++) {
       let row = []
       for(let j = 0; j < this.cols; j++) {
-        row.push(this.tiles.grass)
+        row.push(this.tiles[Math.floor(Math.random() * this.tiles.length)].tile)
       }
       map.push(row)
     }
@@ -219,13 +233,8 @@ class Map {
     this.x += this.vx
     this.y += this.vy
 
-    if(this.x >= 1) {
-      this.x = 1
-    }
-
-    if(this.y >= 1) {
-      this.y = 1
-    }
+    if(this.x >= 1) { this.x = 1 }
+    if(this.y >= 1) { this.y = 1 }
 
     if(this.x <= -(this.width - this.context.canvas.width)) {
       this.x = -(this.width - this.context.canvas.width)
