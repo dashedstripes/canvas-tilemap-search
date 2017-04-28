4bef3dd33d0433ebd8a9c7a3fd59007e3fdd68f3
Adam Gray <adam@pateo.co>
2017-04-28 13:36:56 +0100

    Change font height

--- src/info.js
+++ src/info.js
@@ -6,13 +6,13 @@ class Info {
 
   draw() {
     let textWidth = this.context.measureText(this.text).width
-    let textHeight = 18
+    let textHeight = 16
     
-    this.context.font = "18px Open Sans";
+    this.context.font = "16px Open Sans";
     this.context.fillStyle = '#000000'
     this.context.fillRect(this.context.canvas.width - (textWidth + 20) - 20, 20, textWidth * 2, textHeight * 2)
     this.context.fillStyle = '#ffffff'
-    this.context.fillText(this.text, this.context.canvas.width - (textWidth + 20), 45)
+    this.context.fillText(this.text, this.context.canvas.width - (textWidth + 20), 42)
   }
 }
 
